a57f839344d594f1cc006fb41f433023ef7e3a43
Myles Rademaker <mylesprademaker@gmail.com>
2025-08-08 18:16:48 -0700

    Initial setup of vscode editor settings and godot editor settings

---
+++ scripts/QPointCloudRecognizer.cs
@@ -0,0 +1,22 @@
+// Source code adapted from https://depts.washington.edu/acelab/proj/dollar/qdollar.html
+
+//	Vatavu, R.-D., Anthony, L. and Wobbrock, J.O. (2018).  
+//	  $Q: A Super-Quick, Articulation-Invariant Stroke-Gesture
+//    Recognizer for Low-Resource Devices. Proceedings of 20th International Conference on
+//    Human-Computer Interaction with Mobile Devices and Services (MobileHCI '18). Barcelona, Spain
+//	  (September 3-6, 2018). New York: ACM Press.
+//	  DOI: https://doi.org/10.1145/3229434.3229465
+//
+// This software is distributed under the "New BSD License" agreement:
+
+// Copyright (c) 2018, Radu-Daniel Vatavu, Lisa Anthony, and 
+// Jacob O. Wobbrock. All rights reserved.
+
+
+using Godot;
+
+namespace Game;
+
+public partial class QPointCloudRecognizer : Node {
+
+}
